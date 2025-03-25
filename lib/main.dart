fbb724d133576ab96581e1da0bafae01920f8244
jonas <xy@abc.de>
2025-03-25 16:14:25 +0100

    add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -33,10 +33,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 0;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
@@ -142,11 +149,20 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            if (_selectedIndex == 0)
+              const Text(
+                'Home page',
+              ),
+            if (_selectedIndex == 1)
+              const Text(
+                'Search page',
+              ),
+            if (_selectedIndex == 2)
+              const Text(
+                'Profile page',
+              ),
             Text(
-              '$_counter',
+              '$_selectedIndex',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
@@ -156,6 +172,27 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, // Active tab index
+        onTap: _onItemTapped, // Change tab when clicked
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.blue, // Active tab color
+        unselectedItemColor: Colors.grey, // Inactive tab color
+        showUnselectedLabels: true, // Show labels for all items
       ),
     );
   }
